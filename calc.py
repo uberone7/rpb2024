71b62c71ad25e026614de595fc6504e8f410fcb6
uberone7 <kwakhowon@gmail.com>
2024-04-05 02:27:28 +0900

    the elice ver.

--- calc.py
+++ calc.py
@@ -1,1 +1,23 @@
 def main():
+    print("Let's implement division. Type two numbers for x and y")
+    
+    x = int(input("x > "))
+    y = int(input("y > "))
+    
+    print("%d / %d = %0.3f" % (x, y, divide(x, y)))
+    
+    
+def add(a, b):
+    return a + b
+    
+    
+# TODO: divide() 함수 정의
+def divide(a, b):
+    if b==0:
+        print("Error: cannot divide by zero")
+        exit()
+    else:
+        return a/b
+
+if __name__ == "__main__":
+    main()
